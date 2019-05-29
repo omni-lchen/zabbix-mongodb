4a6c7473eaf200142cb5f69d06df63e74c5325e8
Denis <denisgolius@users.noreply.github.com>
2019-05-29 21:45:35 +0300

    Fix comments by replacing some pirces of code as variables

--- bin/mongodb-stats.sh
+++ bin/mongodb-stats.sh
@@ -6,12 +6,16 @@
 # Requires:             Zabbix Sender, zabbix-mongodb.py
 
 get_MongoDB_metrics(){
-python /usr/local/bin/zabbix-mongodb.py
+    python3 /etc/zabbix/zabbix-mongodb/bin/zabbix-mongodb.py
 }
 
+varA="$1 ~ /^info/ && match($1,/[0-9].*$/)"
+varB="{sum+=substr($1,RSTART,RLENGTH)\} END \{print sum}"
+
 # Send the results to zabbix server by using zabbix sender
-result=$(get_MongoDB_metrics | /usr/bin/zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i - 2>&1)
-response=$(echo "$result" | awk -F ';' '$1 ~ /^info/ && match($1,/[0-9].*$/) {sum+=substr($1,RSTART,RLENGTH)} END {print sum}')
+result=$(get_MongoDB_metrics | /usr/bin/zabbix_sender -vv -c /etc/zabbix/zabbix_agentd.conf -i - 2>&1)
+response=$(echo "$result" | awk -F ";" '$varA $varB')
+
 if [ -n "$response" ]; then
         echo "$response"
 else
