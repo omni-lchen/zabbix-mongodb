5b1be8c3a40521e663d160a4c695fad0f147a668
Gennady Melesh <32384639+gennady-melesh@users.noreply.github.com>
2019-08-13 12:37:46 +0300

    Update mongodb-stats.sh
    Fix for zabbix_sender >= 4.0

--- bin/mongodb-stats.sh
+++ bin/mongodb-stats.sh
@@ -11,7 +11,7 @@ python /usr/local/bin/zabbix-mongodb.py
 
 # Send the results to zabbix server by using zabbix sender
 result=$(get_MongoDB_metrics | /usr/bin/zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i - 2>&1)
-response=$(echo "$result" | awk -F ';' '$1 ~ /^info/ && match($1,/[0-9].*$/) {sum+=substr($1,RSTART,RLENGTH)} END {print sum}')
+response=$(echo "$result" | awk -F ';' '$1 ~ /^sent/ && match($1,/[0-9].*$/) {sum+=substr($1,RSTART,RLENGTH)} END {print sum}')
 if [ -n "$response" ]; then
         echo "$response"
 else
